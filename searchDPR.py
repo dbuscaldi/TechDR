b112f4c2899e13bf06299737e267fed1bdac8510
Davide Buscaldi <dbuscaldi@donnager.local>
2022-01-28 12:13:52 +0100

    added searchDPR

--- searchDPR.py
+++ searchDPR.py
@@ -1,12 +1,26 @@
 import json
 import logging, pickle
 import sys
+import numpy as np
 
 import argparse
 import collections
 from typing import Dict, List, Tuple, Optional
 
+from transformers import DPRQuestionEncoder, DPRQuestionEncoderTokenizer
 
+def cosine(a, b):
+    norm_a = np.linalg.norm(a)
+    norm_b = np.linalg.norm(b)
+    return np.dot(a,b) / (norm_a * norm_b)
+
+def get_Kbest(index, query_emb, K):
+    distances = []
+    for (doc, emb) in index:
+        score = cosine(emb, query_emb)
+        distances.append((doc, score))
+    distances.sort(key = lambda t : t[1])
+    return distances[:K]
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -24,39 +38,46 @@ def parse_args():
         sys.exit(1)
     return parser.parse_args()
 
+print("loading model...")
+tokenizer = DPRQuestionEncoderTokenizer.from_pretrained('facebook/dpr-question_encoder-single-nq-base')
+model = DPRQuestionEncoder.from_pretrained('facebook/dpr-question_encoder-single-nq-base')
+print("done.")
+
 def main(OPTS):
     logging.basicConfig(level=OPTS.verbose)
 
     out_pred={}
     predictions={}
-    index = pickle.load(OPTS.index)
+    index = pickle.load(OPTS.index) # (id, embedding) list
+
     with open(OPTS.q_file, encoding='utf-8') as f:
         dataset = {query['QUESTION_ID']: query for query in json.load(f)}
-
-
         for qid, q in dataset.items():
             txt=q['QUESTION_TITLE']
             answerable=q['ANSWERABLE']
             #if answerable == 'N': continue
             print(qid)
             predictions[qid]=list()
-            with ix.searcher() as searcher:
-                query = QueryParser("body", ix.schema).parse(txt)
-                results = searcher.search(query)
-                pred={}
-                if len(results) == 0 :
-                    #No answer Case
-                    pred["doc_id"]=""
-                    pred["score"]=0
+
+            input_ids = tokenizer(txt, return_tensors='pt')["input_ids"]
+            query_embeddings = model(input_ids).pooler_output
+
+            results=get_Kbest(index, query_embeddings, K=5)
+
+            pred={}
+            if len(results) == 0 :
+                #No answer Case (should never happen)
+                pred["doc_id"]=""
+                pred["score"]=0
+                predictions[qid].append(pred)
+            else:
+                i=1
+                for r in results:
+                    #print(i, r['doc_id'], r.score)
+                    pred["doc_id"]=r[0]
+                    pred["score"]=r[1]
                     predictions[qid].append(pred)
-                else:
-                    i=1
-                    for r in results:
-                        #print(i, r['doc_id'], r.score)
-                        pred["doc_id"]=r['doc_id']
-                        pred["score"]=r.score
-                        predictions[qid].append(pred)
-                        i+=1
+                    i+=1
 
     out_pred["predictions"]=predictions
     if OPTS.out_file:
