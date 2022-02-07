44cf6c5d2911b9921ca6f6480181adb78b669444
dbuscaldi <davide.buscaldi@gmail.com>
2022-02-07 12:25:59 +0100

    Removed duplicate results

--- searchDPR.py
+++ searchDPR.py
@@ -1,7 +1,9 @@
-import json
+import json, bz2
 import logging, pickle
 import sys
 import numpy as np
+
+from bs4 import BeautifulSoup
 
 import argparse
 import collections
@@ -15,12 +17,20 @@ def cosine(a, b):
     return np.dot(a,b) / (norm_a * norm_b)
 
 def get_Kbest(index, query_emb, K):
-    distances = []
+    distances = {}
     for (doc, emb) in index:
         score = cosine(emb, query_emb)
-        distances.append((doc, score))
-    distances.sort(key = lambda t : -t[1])
-    return distances[:K]
+        try :
+            max_score = distances[doc]
+            if score > max_score:
+                distances[doc]=score
+        except KeyError:
+            distances[doc]=score
+        #distances.append((doc, score))
+    res = sorted(distances.items(), key=lambda item: -item[1])
+    #distances.sort(key = lambda t : -t[1])
+    #return distances[:K]
+    return res[:K]
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -42,7 +52,17 @@ print("loading model...")
 tokenizer = AutoTokenizer.from_pretrained('facebook/dpr-question_encoder-single-nq-base')
 model = DPRQuestionEncoder.from_pretrained('facebook/dpr-question_encoder-single-nq-base')
 print("done.")
-
+"""
+#for debug:
+inf = bz2.BZ2File("../training_dev_technotes.json.bz2", 'r')
+collection={}
+docs = json.load(inf)
+for k in docs.keys():
+    data=docs[k]["content"]
+    soup = BeautifulSoup(data, 'html.parser')
+    text=soup.get_text()
+    collection[k]=text
+"""
 def main(OPTS):
     logging.basicConfig(level=OPTS.verbose)
 
@@ -63,9 +83,14 @@ def main(OPTS):
 
             input_ids = tokenizer(txt, return_tensors='pt')["input_ids"]
             query_embeddings = model(input_ids).pooler_output
-
+            #print("Q: ", txt)
             results=get_Kbest(index, query_embeddings[0].detach().numpy(), K=5)
-
+            """
+            for r in results:
+                print(r)
+                print(collection[r[0]])
+                print('------------------------------------')
+            """
             pred={}
             if len(results) == 0 :
                 #No answer Case (should never happen)
